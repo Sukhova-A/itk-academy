9a74576616f7e86797ee8da0830a97d872b95595
Anastasiia Sukhova <gvozdeva.anastas@gmail.com>
2026-03-02 16:33:09 +0400

    init commit

---
+++ src/main/java/com/anastasiia/itkacademy/ItkAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.anastasiia.itkacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItkAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItkAcademyApplication.class, args);
+    }
+
+}
