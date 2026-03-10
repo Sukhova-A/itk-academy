b30885588314d6b3086c2f10fea089b19ca312d0
Anastasiia Sukhova <gvozdeva.anastas@gmail.com>
2026-03-10 13:19:39 +0400

    Вычисление средних оценок по предметам

--- src/main/java/com/anastasiia/itkacademy/ItkAcademyApplication.java
+++ src/main/java/com/anastasiia/itkacademy/ItkAcademyApplication.java
@@ -1,13 +1,31 @@
 package com.anastasiia.itkacademy;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.anastasiia.itkacademy.task8.Student;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ItkAcademyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ItkAcademyApplication.class, args);
-    }
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
 
+        Map<String, Double> averageGrades = students
+                .parallelStream()
+                .flatMap(student -> student.getGrades().entrySet().stream())
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        Collectors.averagingInt(Map.Entry::getValue)));
+
+        averageGrades.forEach((key, value) -> System.out.printf("Средняя оценка по предмету \"%s\": %.1f%n", key, value));
+    }
 }
