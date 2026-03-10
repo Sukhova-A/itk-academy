7e8ebf0903f4ad73895f8482a3e5bad8348aecbd
Anastasiia Sukhova <gvozdeva.anastas@gmail.com>
2026-03-10 17:55:57 +0400

    Вычисление факториала с использованием ForkJoinPool

--- src/main/java/com/anastasiia/itkacademy/ItkAcademyApplication.java
+++ src/main/java/com/anastasiia/itkacademy/ItkAcademyApplication.java
@@ -1,13 +1,21 @@
 package com.anastasiia.itkacademy;
 
-import org.springframework.boot.SpringApplication;
+import java.util.concurrent.ForkJoinPool;
+
+import com.anastasiia.itkacademy.task9.FactorialTask;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ItkAcademyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ItkAcademyApplication.class, args);
-    }
+        int n = 10; // Вычисление факториала для числа 10
 
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        FactorialTask factorialTask = new FactorialTask(n);
+
+        long result = forkJoinPool.invoke(factorialTask);
+
+        System.out.println("Факториал " + n + "! = " + result);
+    }
 }
