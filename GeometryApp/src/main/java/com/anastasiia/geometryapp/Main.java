ee43081b4b4b094f9557b80203c360fc75839787
Anastasiia Sukhova <gvozdeva.anastas@gmail.com>
2026-03-13 18:55:06 +0400

    2. Создание GeometryApp, использующий GeometryLibrary. Создание объектов геометрических фигур из GeometryLibrary

---
+++ GeometryApp/src/main/java/com/anastasiia/geometryapp/Main.java
@@ -0,0 +1,28 @@
+package com.anastasiia.geometryapp;
+
+import com.anastasiia.geometry.Circle;
+import com.anastasiia.geometry.Rectangle;
+import com.anastasiia.geometry.Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Triangle triangle = new Triangle(3.0, 4.0, 5.0);
+
+        System.out.println("Circle: " + circle);
+        System.out.println("Area: " + circle.getArea());
+        System.out.println("Perimeter: " + circle.getPerimeter());
+        System.out.println();
+
+        System.out.println("Rectangle: " + rectangle);
+        System.out.println("Area: " + rectangle.getArea());
+        System.out.println("Perimeter: " + rectangle.getPerimeter());
+        System.out.println();
+
+        System.out.println("Triangle: " + triangle);
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+    }
+}
